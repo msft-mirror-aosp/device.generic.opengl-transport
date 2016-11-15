7858837985f5ab26a131d461b14c63521a31e9e9
David 'Digit' Turner <digit@google.com>
2016-11-15 17:23:06 +0100

    emugl: Move IOStream.h to host/include/libOpenglRender/
    There is no longer a reason to distinguish between include/libOpenglRender
    and include/OpenglRender/. Since the former only contains IOStream.h,
    move the header to include/libOpenglRender to get rid of one include
    directory during the build.
    
    Change-Id: I75b20025e79d25ed9dfb1c6e57c55566a5995c17

--- distrib/android-emugl/host/libs/libOpenglRender/ReadBuffer.h
+++ distrib/android-emugl/host/libs/libOpenglRender/ReadBuffer.h
@@ -13,16 +13,14 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#ifndef _READ_BUFFER_H
-#define _READ_BUFFER_H
-
-#include "IOStream.h"
+#pragma once
+#include "OpenglRender/IOStream.h"
 
 class ReadBuffer {
 public:
-    ReadBuffer(IOStream *stream, size_t bufSize);
+    explicit ReadBuffer(size_t bufSize);
     ~ReadBuffer();
-    int getData(); // get fresh data from the stream
+    int getData(IOStream *stream, int minSize); // get fresh data from the stream
     unsigned char *buf() { return m_readPtr; } // return the next read location
     size_t validData() { return m_validData; } // return the amount of valid data in readptr
     void consume(size_t amount); // notify that 'amount' data has been consumed;
@@ -31,6 +29,4 @@ private:
     unsigned char *m_readPtr;
     size_t m_size;
     size_t m_validData;
-    IOStream *m_stream;
 };
-#endif
