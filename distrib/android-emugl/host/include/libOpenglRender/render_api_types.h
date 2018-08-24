aosp-mirror/platform_external_qemu

[gl] New opengl rendering library interface
The CL creates a new C++-based interface for the GPU emulation
library. The only remaining C function is initLibrary which
returns and instance of IRenderLib interface.

New interface is type and memory safe, and contains of the
following objects:

IRenderLib - a root object, creates instances of IRenderer
IRenderer - preconfigured object that manages a surface to draw
  into and can create IRenderingChannel-s
IRenderingChannel - an object that supports the guest's GPU
  emulation protocol and performs the actual GL operations.
  It is supposed to be used by a RenderThread to channel data
  from and to the guest

This CL isn't 'complete' on its own - it just provides new
interface specification. Implementation to follow.

Change-Id: I18f11586401c9f9a3a7affab10421534f6b2f0cb

distrib/android-emugl/host/include/OpenglRender/render_api_types.h
@@ -1,0 +1,23 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+typedef void (*crash_reporter_t)(const char* format, ...);
+
+typedef void (*logger_t)(const char* fmt, ...);
+typedef struct {
+    logger_t coarse;
+    logger_t fine;
+} emugl_logger_struct;
+

msft-mirror-aosp/device.generic.opengl-transport

distrib/android-emugl/host/include/libOpenglRender/render_api_types.h
@@ -1,0 +1,23 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#pragma once
+
+typedef void (*emugl_crash_reporter_t)(const char* format, ...);
+
+typedef void (*emugl_logger_t)(const char* fmt, ...);
+typedef struct {
+    emugl_logger_t coarse;
+    emugl_logger_t fine;
+} emugl_logger_struct;
+